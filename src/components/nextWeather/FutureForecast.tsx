231907a8e21c6268d1c2f8ec826d5c01d8402c7d
sepehrnava <sepehrnava@gmail.com>
2024-04-27 03:14:43 +0100

    add 5day forecast layout with Skeleton

--- src/components/nextWeather/FutureForecast.tsx
+++ src/components/nextWeather/FutureForecast.tsx
@@ -1,12 +1,7 @@
 import type { AvailableUnits } from "@/components/currentWeather/Tempreature";
-import { Card, CardContent } from "@/components/ui/card";
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-} from "@/components/ui/carousel";
-import Marquee from "@/components/ui/Marquee";
 import type { FutureForecastAPI } from "@/types/weather/FutureForecast";
+
+import FutureForecastLayout from "./FutureForecastLayout";
 
 async function get5DaysForecast(locationKey: string, unit?: AvailableUnits) {
   let data: FutureForecastAPI;
@@ -35,53 +30,5 @@ interface FutureForecastProps {
 
 export default async function FutureForecast(props: FutureForecastProps) {
   const forecasts = await get5DaysForecast(props.locationKey, props.unit);
-  return (
-    <div>
-      <Carousel>
-        <CarouselContent className="lg:justify-center">
-          {forecasts.DailyForecasts.map((forecast, index) => (
-            <CarouselItem
-              key={index}
-              className="h-[395px] max-w-[172px] md:basis-1/3 lg:basis-1/4 xl:basis-1/5"
-            >
-              <div className="size-full">
-                <Card className="cardGradient size-full !border-none !outline-none">
-                  <CardContent className="flex size-full flex-col justify-end p-4">
-                    <p className="text-2xl font-black text-white">
-                      {new Date(forecast.Date)
-                        .toLocaleString("en-US", {
-                          weekday: "short",
-                          day: "numeric",
-                        })
-                        .split(" ")
-                        .reverse()
-                        .join(" ")
-                        .toUpperCase()}
-                    </p>
-                    <div className="flex w-full items-center justify-between">
-                      <p className="!mt-0 ml-[-7px] text-5xl font-black text-primary">
-                        {forecast.Temperature.Maximum.Value.toFixed(0)}°
-                      </p>
-                      <p className="!mt-3 mr-[-9px] text-4xl font-black">
-                        {forecast.Temperature.Minimum.Value.toFixed(0)}°
-                      </p>
-                    </div>
-
-                    <Marquee className="!mt-0">
-                      {forecast.Day.PrecipitationType ||
-                        forecast.Day.IconPhrase.split(" ")
-                          .slice(0, 2)
-                          .join(" ")}
-                    </Marquee>
-                  </CardContent>
-                </Card>
-              </div>
-            </CarouselItem>
-          ))}
-        </CarouselContent>
-        {/* <CarouselPrevious />
-        <CarouselNext /> */}
-      </Carousel>
-    </div>
-  );
+  return <FutureForecastLayout forecasts={forecasts} />;
 }
--- src/components/nextWeather/FutureForecastLayout.tsx
+++ src/components/nextWeather/FutureForecastLayout.tsx
@@ -1,5 +1,74 @@
 import React from "react";
 
-export default function FutureForecastLayout() {
-  return <div>FutureForecastLayout</div>;
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from "@/components/ui/carousel";
+import Marquee from "@/components/ui/Marquee";
+
+import type { FutureForecastAPI } from "../../types/weather/FutureForecast";
+import { Skeleton } from "../ui/skeleton";
+
+interface FutureForecastLayoutProps {
+  forecasts?: FutureForecastAPI;
+}
+
+export default function FutureForecastLayout(
+  props?: FutureForecastLayoutProps,
+) {
+  const DailyForecasts = props?.forecasts?.DailyForecasts || [];
+
+  return (
+    <Carousel>
+      <CarouselContent className="lg:justify-center">
+        {DailyForecasts.length > 0 ? (
+          DailyForecasts.map((forecast, index) => (
+            <CarouselItem
+              key={index}
+              className="h-[395px] max-w-[172px] md:basis-1/3 lg:basis-1/4 xl:basis-1/5"
+            >
+              <div className="size-full">
+                <Card className="cardGradient size-full !border-none !outline-none">
+                  <CardContent className="flex size-full flex-col justify-end p-4">
+                    <p className="text-2xl font-black text-white">
+                      {new Date(forecast.Date)
+                        .toLocaleString("en-US", {
+                          weekday: "short",
+                          day: "numeric",
+                        })
+                        .split(" ")
+                        .reverse()
+                        .join(" ")
+                        .toUpperCase()}
+                    </p>
+                    <div className="flex w-full items-center justify-between">
+                      <p className="!mt-0 ml-[-7px] text-5xl font-black text-primary">
+                        {forecast.Temperature.Maximum.Value.toFixed(0)}°
+                      </p>
+                      <p className="!mt-3 mr-[-9px] text-4xl font-black">
+                        {forecast.Temperature.Minimum.Value.toFixed(0)}°
+                      </p>
+                    </div>
+
+                    <Marquee className="!mt-0">
+                      {forecast.Day.PrecipitationType ||
+                        forecast.Day.IconPhrase.split(" ")
+                          .slice(0, 2)
+                          .join(" ")}
+                    </Marquee>
+                  </CardContent>
+                </Card>
+              </div>
+            </CarouselItem>
+          ))
+        ) : (
+          <Skeleton className="h-[395px] w-[calc(5_*_172px)]" />
+        )}
+      </CarouselContent>
+      {/* <CarouselPrevious />
+        <CarouselNext /> */}
+    </Carousel>
+  );
 }
